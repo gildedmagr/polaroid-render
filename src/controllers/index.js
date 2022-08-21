a27723259af14abb85551a9b0610a5b6c3cea92e
Andrii Protsenko <aprotsenko@liveperson.com>
2022-08-22 00:59:47 +0300

    Update controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -10,7 +10,8 @@ const render = async (req, res, next) => {
         const uid = req.query.uid;
         const pages = req.query.pages;
         const type = req.query.type;
-        res.json(await renderService.startRender(domain, uid, pages, type || 'polaroid'));
+        renderService.startRender(domain, uid, pages, type || 'polaroid');
+        res.json({'status': 'launched'});
     } catch (err) {
         console.error(`Error while getting programming languages`, err.message);
         next(err);
