a421f4e79a9143c55758cc56d8f14499c50d50ba
Andrii Protsenko <aprotsenko@liveperson.com>
2022-08-21 13:27:20 +0300

    Add support for multiple copies

--- src/services/render.service.js
+++ src/services/render.service.js
@@ -3,9 +3,10 @@ const placeholdify = require('placeholdify');
 const fs = require('fs');
 
 const polaroidRenderPage = 'https://{0}/index.php?route=polaroid/polaroid/renderPolaroid&uid={1}&page={2}&type={3}';
-let domains = fs.readFileSync(process.env.DOMAINS_DICT_PATH || 'domains.json');
-let domainsMap = JSON.parse(domains);
+const domains = fs.readFileSync(process.env.DOMAINS_DICT_PATH || 'domains.json');
+const domainsMap = JSON.parse(domains);
 const relativeDestinationPath = 'image/polaroid';
+const isProd = process.env.NODE_ENV === 'production';
 
 const startRender = async (domain, uid, pages, type) => {
     const start = Date.now();
@@ -19,32 +20,58 @@ const startRender = async (domain, uid, pages, type) => {
     await (async () => {
         const browser = await puppeteer.launch(
             {
-                executablePath: '/usr/bin/google-chrome',
+                executablePath: isProd ? '/usr/bin/google-chrome' : '',
                 headless: true,
                 ignoreHTTPSErrors: true,
                 args: [
                     `--window-size=${browserWidth},${browserHeight}`,
-                    '--no-sandbox'
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox'
                 ],
+                dumpio: false,
                 defaultViewport: {
                     width: browserWidth,
                     height: browserHeight
                 }
             }
         );
+
+        let copies = 1;
+
         const page = await browser.newPage();
+        page.on('console', msg => {
+            const message = msg.text();
+            if(message.startsWith('copies:')){
+                copies = parseInt(msg.text().replace('copies:', ''));
+            }
+        });
+
+
+        //const copies =3;
         for (let i = 0; i < pages; i++) {
             const url = placeholdify(polaroidRenderPage, domain, uid, i, type);
-            console.log(`Going to create snapshot from: ${url}`)
+            console.log(`Going to create snapshot from: ${url}`);
+
+
+
+            const destFile = `${destinationPath}/${i}.jpg`;
             await page.goto(url);
             await page.screenshot({
-                path: `${destinationPath}/${i}.jpg`,
+                path: destFile,
                 type: 'jpeg',
                 quality: 100
             });
             console.log('Snapshot has been created');
-            links.push(`${domain}/${relativeDestinationPath}/${uid}/${i}.jpg`);
+            if(copies > 1){
+                console.log(`Polaroid has ${copies} copies. Going to duplicate final image`);
+                for(let copy = 1; copy < copies; copy++){
+                    const copyFile = `${destinationPath}/${i}_copy_${copy}.jpg`
+                    fs.copyFileSync(destFile, copyFile);
+                }
+            }
+            links.push(`https://${domain}/${relativeDestinationPath}/${uid}/${i}.jpg`);
         }
+        await page.close();
         await browser.close();
 
     })();
