b1cc7a162993d78f4ebd6c2d7a0c12048e72efc7
Andrii Protsenko <aprotsenko@liveperson.com>
2022-08-26 14:09:29 +0300

    Add cors

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 const route = require('./src/routes')
-const {testRender} = require('./src/services')
+const fs = require("fs");
+const domainsFile = fs.readFileSync(process.env.DOMAINS_DICT_PATH || 'domains.json');
+const domainsMap = JSON.parse(domainsFile);
+let domains = Object.keys( domainsMap );
+console.log(domains);
+
+app.use(cors({
+    origin: domains
+}));
 
 app.use(bodyParser.json());
 app.use(
@@ -13,8 +22,6 @@ app.use(
 );
 
 app.use(route);
-
-//testRender();
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`Render is running at http://localhost:${port}`)
