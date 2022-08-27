63fb92d9cd9c4f05732ad1512ce55e3e80c4aa33
Andrii Protsenko <aprotsenko@liveperson.com>
2022-08-27 09:51:02 +0300

    WIP

--- src/services/polaroid.service.js
+++ src/services/polaroid.service.js
@@ -92,16 +92,51 @@ const testRender = async (domain, uid, totalPages, userPreview) => {
     if (!fs.existsSync(destinationPath)) {
         fs.mkdirSync(destinationPath, {recursive: true});
     }
+
+    const minimal_args = [
+        '--autoplay-policy=user-gesture-required',
+        '--disable-background-networking',
+        '--disable-background-timer-throttling',
+        '--disable-backgrounding-occluded-windows',
+        '--disable-breakpad',
+        '--disable-client-side-phishing-detection',
+        '--disable-component-update',
+        '--disable-default-apps',
+        '--disable-dev-shm-usage',
+        '--disable-domain-reliability',
+        '--disable-extensions',
+        '--disable-features=AudioServiceOutOfProcess',
+        '--disable-hang-monitor',
+        '--disable-ipc-flooding-protection',
+        '--disable-notifications',
+        '--disable-offer-store-unmasked-wallet-cards',
+        '--disable-popup-blocking',
+        '--disable-print-preview',
+        '--disable-prompt-on-repost',
+        '--disable-renderer-backgrounding',
+        '--disable-setuid-sandbox',
+        '--disable-speech-api',
+        '--disable-sync',
+        '--hide-scrollbars',
+        '--ignore-gpu-blacklist',
+        '--metrics-recording-only',
+        '--mute-audio',
+        '--no-default-browser-check',
+        '--no-first-run',
+        '--no-pings',
+        '--no-sandbox',
+        '--no-zygote',
+        '--password-store=basic',
+        '--use-gl=swiftshader',
+        '--use-mock-keychain',
+        `--window-size=${browserWidth},${browserHeight}`,
+    ];
     const browser = await puppeteer.launch(
         {
             executablePath: isProd ? '/usr/bin/google-chrome' : '',
             headless: true,
             ignoreHTTPSErrors: true,
-            args: [
-                `--window-size=${browserWidth},${browserHeight}`,
-                '--no-sandbox',
-                '--disable-setuid-sandbox'
-            ],
+            args: minimal_args,
             dumpio: false,
             defaultViewport: {
                 width: browserWidth,
@@ -139,13 +174,11 @@ const testRender = async (domain, uid, totalPages, userPreview) => {
             width: viewPortWidth,
             height: browserHeight
         })
-        emit(uid, 'progress', Math.round((100 / totalPages * currentPage) * 0.33));
         await page.screenshot({
             path: destFile,
             type: 'jpeg',
             quality: 100
         });
-        emit(uid, 'progress', Math.round((100 / totalPages * currentPage) * 0.66));
         console.log('Snapshot has been created');
         let image = await Image.load(destFile);
         let borderSize = currentPage === 1 ? Math.round(viewPortWidth / 100 * 3) : Math.round(image.height - image.height / 1.02040);
