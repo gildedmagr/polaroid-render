a4244a523ad809d86bb84fc045abef0f041dee86
Andrii Protsenko <aprotsenko@liveperson.com>
2022-09-23 17:14:14 +0300

    Remove unused code

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -18,27 +18,7 @@ const render = async (req, res, next) => {
     }
 }
 
-const render2 = async (req, res, next) => {
-    try {
-        const domain = req.query.domain;
-        const uid = req.query.uid;
-        const pages = req.query.pages;
-        const userPreview = req.query.userPreview;
-        console.log(userPreview);
-        if(userPreview === 'false'){
-            const result = await renderService.testRender(domain, uid, pages, userPreview);
-            res.json(result);
-        }else{
-            res.json([]);
-        }
-    } catch (err) {
-        console.error(`Error while getting programming languages`, err.message);
-        next(err);
-    }
-}
-
 module.exports = {
     healthStatus,
-    render2,
     render
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const controller = require('../controllers/index')
 
 router.get('/polaroid/render/start', controller.render);
-router.get('/photobook/render/start', controller.render2);
 router.get('/health/status', controller.healthStatus);
 
 module.exports = router;
